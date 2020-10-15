d4320f800d499f25d662cb8fe98c098d861dc56e
Joao Henrique <404joaohbatista@gmail.com>
2020-10-15 14:42:23 -0300

    Adicionei novos campos no cadastro

--- formCadastro.php
+++ formCadastro.php
@@ -5,20 +5,23 @@ require_once("conexao.php");
 $nome = $_POST['username'];
 $email = $_POST['email'];
 $senha = $_POST['password'];
-$cnpj = $_POST['cnpj'];
-$razao = $_POST['razao'];
 $confSenha = $_POST['confirm-password'];
+$cnpj = $_POST['cnpj'];
+$rua = $_POST['rua'];
+$cep = $_POST['cep'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$razao = $_POST['razao'];
+
 
 if($senha == $confSenha){
 
-    $sql = "insert into instituicoes values(null, '".$nome."', '".$email."', '".$senha."', '".$razao."', '".$cnpj."')";
+    $sql = "insert into instituicoes values(null, '".$nome."', '".$email."', '".$senha."', '".$cnpj."', '".$rua."', '".$cep."', '".$cidade."', '".$estado."', '".$razao."', )";
     $salvar = mysqli_query($conexao, $sql);
 
 }else{
     header('location: LoginCadastro.html');   
     exit;
-    
-
 }
 
 mysqli_close($conexao);
